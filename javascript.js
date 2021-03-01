c4c0502c2514db2904ac64e29b2b2da57cbdee17
ajaysaini99 <65387534+ajaysaini99@users.noreply.github.com>
2021-03-01 21:24:55 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -9,3 +9,54 @@ function change(element)
 
 }
 
+
+function changeBg(){
+
+   var pics = [
+      'url("/bgImages/welcome.jpg")',
+      'url("/bgImages/burano.jpg")',
+      'url("/bgImages/homecar.jpg")',
+      'url("/bgImages/blue-builds.jpg")',
+      'url("/bgImages/white-houses.jpg")',
+      'url("/bgImages/door.jpg")',
+   ]
+   let i=0;
+   var bg = document.getElementById('welcomeId');
+   var h1 = document.getElementById('welcomeId').getElementsByTagName('h1')[0];
+   var para = document.getElementById('welcomeId').getElementsByTagName('p')[0];
+   function swap_Images(){
+      i = i % pics.length;
+      bg.style.backgroundImage = pics[i];
+     
+     
+      bg.style.transitionDuration = '4s';
+      i++;
+   }
+   
+   setInterval(swap_Images, 4000);
+
+}
+changeBg();
+
+var timer_id;
+function keep_changing(pics)
+{
+   var section = document.getElementById('section-1');
+   var curr_img = section.getElementsByTagName('img')[0];
+   let i=0;
+
+   function run_image(){
+      i = i % pics.length;
+      curr_img.src = pics[i];
+      // bg.style.transitionDuration = '4s';
+      i++;
+   }
+   
+    timer_id = setInterval(run_image, 2000);
+
+}
+
+function stop_changing(){
+
+   clearInterval(timer_id);
+}
